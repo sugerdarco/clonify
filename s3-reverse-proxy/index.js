4168551cbdf23df444d864f5dec7a0c888401cd7
sugerdarco <thoratbkumar@gmail.com>
2025-02-16 22:23:43 +0530

    add reverse-proxy

---
+++ s3-reverse-proxy/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import httpProxy from 'http-proxy';
+
+const app = express();
+const port = 8000;
+const Base_path = '' // s3 bucket base address
+
+const proxy = httpProxy.createRightProxy()
+
+app.use((req, res) => {
+  const hostname = req.hostname;
+  const subdomain = hostname.substring('.')[0];
+
+  // Db query to fetch site
+  const resolveTo = `${Base_path}/${subdomain}`;
+  return proxy.web(req, res, {
+    target: resolveTo,
+    changeOrigin: true,
+  })
+});
+
+proxy.on('proxyReq', (proxyReq, req, res) => {
+  const url = req.url;
+  if (url == '/') {
+    proxyReq.path += 'index.html';
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Reverse Proxy Server listening on port ${port}`)
+});
